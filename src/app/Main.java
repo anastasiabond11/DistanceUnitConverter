7c9e27ddcdf9a6ac01f8e1b9cfdefc9a3c1d87d0
Anastasiia Bondarenko <bondarenkonastya02@icloud.com>
2024-06-13 17:49:36 +0200

    Add conversion of kilometres to miles

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,9 +3,14 @@ package app;
 public class Main {
 
     private static final double MILES_TO_KILOMETERS = 1.60934;
+    private static final double KILOMETERS_TO_MILES = 1 / MILES_TO_KILOMETERS;
 
     public static double convertMilesToKilometers(double miles) {
         return miles * MILES_TO_KILOMETERS;
+    }
+
+    public static double convertKilometersToMiles(double kilometers) {
+        return kilometers * KILOMETERS_TO_MILES;
     }
 
     public static void main(String[] args){
@@ -13,7 +18,9 @@ public class Main {
 
         double miles = 5;
         double kilometers = convertMilesToKilometers(miles);
+        double convertedMiles = convertKilometersToMiles(kilometers);
         System.out.printf("%.2f miles is equal to %.2f kilometers%n", miles, kilometers);
+        System.out.printf("%.2f kilometers is equal to %.2f miles%n", kilometers, convertedMiles);
     }
 }
 
